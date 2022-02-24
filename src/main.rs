48e516301db2cfaef3173c0d5058c3fd7a1356d1
Johan Planchon <johan.planchon@simplx.fr>
2022-02-24 14:52:44 +0100

    [WIP] Basic functional logic in -> process -> out

--- src/main.rs
+++ src/main.rs
@@ -26,23 +26,63 @@ fn main() -> EyreResult<()> {
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(untagged)]
 enum PackageDefinition {
     Raw(PackageRaw),
     WithManager(PackageWithManager),
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, Default)]
+struct PackagesDefinitions(Vec<PackageDefinition>);
+
+impl PackagesDefinitions {
+    pub fn get_from_stdio() -> Self {
+        let stdin = io::stdin();
+        let mut stdin_data = String::new();
+
+        let mut line = String::new();
+        while let Ok(nb_bytes) = stdin.read_line(&mut line) {
+            if nb_bytes == 0 {
+                break;
+            };
+            stdin_data.push_str(&line);
+            line.clear();
+        }
+
+        dbg!(&stdin_data);
+
+        match serde_json::from_str::<Self>(&stdin_data) {
+            Ok(packages_defs) => packages_defs,
+            Err(e) => {
+                dbg!(e);
+                PackagesDefinitions::default()
+            }
+        }
+    }
+
+    pub fn to_json(&self) -> String {
+        match serde_json::to_string(&self) {
+            Ok(json) => json,
+            Err(_) => String::from("[]"),
+        }
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 struct PackageRaw {
     name: String,
     cmd: String,
-    os: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    os: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 struct PackageWithManager {
     manager: PackageManager,
-    os: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    os: Option<String>,
     name: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
     version: Option<String>,
 }
 
@@ -57,25 +97,8 @@ enum PackageManager {
 fn handle_define_package(args: DefinePackageArgs) -> EyreResult<()> {
     setup_logging(args.verbose)?;
 
-    let previous_packages_definitions = if args.stdio {
-        let stdin = io::stdin();
-        let mut stdin_data = String::new();
-
-        let mut line = String::new();
-        while let Ok(nb_bytes) = stdin.read_line(&mut line) {
-            if nb_bytes == 0 {
-                break;
-            };
-            stdin_data.push_str(&line);
-            line.clear();
-        }
-
-        stdin_data
-    } else {
-        String::new()
-    };
-
-    dbg!(previous_packages_definitions);
+    let packages = PackagesDefinitions::get_from_stdio();
+    println!("{}", packages.to_json());
 
     Ok(())
 }
